c2ce5a1144eeafefba0fafebca98f3e328556e44
sichengxia <sichengxia66@gmail.com>
2026-03-30 16:52:33 +0800

    输出解析器

---
+++ component/outputparsers/json_output.py
@@ -0,0 +1,35 @@
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from typing import Optional
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+model = ChatOpenAI(model="deepseek-chat", base_url="https://api.deepseek.com/v1")
+
+# 定义输出结构：Pydantic类
+class Joke(BaseModel):
+    """
+    给⽤⼾讲⼀个笑话。
+    """ 
+    setup: str = Field(description="这个笑话的开头")
+    punchline: str = Field(description="这个笑话的妙语")
+    rating: Optional[int] = Field(
+    default=None, description="从1到10分，给这个笑话评分"
+)
+
+# 定义输出解析器
+# 也可以解析为json，但json shcema并不好写，所以langchain提供了JsonOutputParser，直接传入pydantic类就可以了
+parser = JsonOutputParser(pydantic_object=Joke)
+# print(parser.get_format_instructions())
+
+# 定义提示词模板
+prompt = ChatPromptTemplate.from_template(
+    template="Answer the user query.\n{format_instructions}\n{query}\n",
+    partial_variables={"format_instructions": parser.get_format_instructions()}, # 将格式化说明作为部分变量传入提示词模板，不用每次调用时都传入
+)
+
+# 定义链
+chain = prompt | model | parser
+# 调用链
+result = chain.invoke({"query": "讲一个笑话"})
+print(result)
